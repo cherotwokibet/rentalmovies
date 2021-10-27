498e26940d1321e99711cb54fb00e863a395f2c1
cherotwokibet <bkcherotwo@gmail.com>
2021-10-27 15:20:01 +0300

    first commit

---
+++ startup/db.js
@@ -0,0 +1,10 @@
+const winston = require('winston');
+const mongoose = require('mongoose');
+const config = require('config');
+
+module.exports = function() {
+  const db = config.get('db');
+  mongoose.connect(config.get('db'))
+    .then(() => winston.info(`Connected to ${db}...`));
+    //.catch((err)=>{ new Error(err.message)})
+}
